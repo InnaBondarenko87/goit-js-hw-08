9b7b3052f8ab592e3dc33874a23238f4ea461092
InnaBondarenko87 <bondarenko211287@gmail.com>
2023-12-13 11:15:16 +0100

    upDate

--- js/task-03.js
+++
@@ -1,6 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-inputEl.addEventListener("input", (event) => {
-  const eventEl = inputEl.value.trim();
-  outputEl.textContent = eventEl === "" ? "Anonymous" : eventEl;
-});
